f7393082f3142f5f0436bf19208e1b2c88e733ae
Treidex <treidexsemextetry@gmail.com>
2021-02-26 17:15:34 -0800

    ok, ok

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Instance { get; private set; }
+
+	public Camera MainCamera;
+
+	private void Awake()
+	{
+		if (Instance is null)
+			Instance = this;
+		else
+			Debug.LogError("Instance already exists!");
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField]
+	private float m_Speed;
+	private Rigidbody2D m_Rigidbody;
+
+	private bool m_MouseOver;
+	private bool m_Frozen;
+
+	private Vector2 m_Velocity;
+
+	private void Start()
+	{
+		m_Rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+    private void FixedUpdate()
+	{
+		if (m_MouseOver && Input.GetMouseButton(0))
+		{
+			if (!m_Frozen)
+				OnMouseDown();
+		}
+		else if (m_Frozen)
+			OnMouseUp();
+
+		if (!m_Frozen)
+		{
+			float velHor = Input.GetAxis("Horizontal");
+			float velVer = Input.GetAxis("Vertical");
+
+			m_Rigidbody.AddForce(new Vector2(velHor, velVer) * m_Speed);
+		}
+		else
+		{
+		}
+
+		m_MouseOver = false;
+	}
+
+    private void OnMouseOver() =>
+		m_MouseOver = true;
+
+    private void OnMouseDown()
+	{
+		m_Frozen = true;
+
+		// Save velocity
+		m_Rigidbody.velocity = Vector2.zero;
+		m_Velocity = m_Rigidbody.velocity;
+	}
+
+	private void OnMouseUp()
+	{
+		m_Frozen = false;
+
+		// Restore velocity
+		m_Rigidbody.velocity = m_Velocity;
+	}
+}
